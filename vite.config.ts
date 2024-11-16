7afc78050818166a213d21f5bd2dd6cc63883123
Mohammad Amrollahi <mohammadamrollahi3@gmail.com>
2024-11-16 12:23:03 +0330

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/setareyek-pwa-test/"
-
-})
+  base: "/setareyek-pwa-test/",
+  build: {
+    chunkSizeWarningLimit: 5000,
+  },
+});
